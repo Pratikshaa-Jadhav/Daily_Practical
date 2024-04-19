f873dc0177db1764eb0f9fafbf572b0a6e2c1b18
Pratikshaa-Jadhav <127178027+Pratikshaa-Jadhav@users.noreply.github.com>
2024-04-19 15:44:48 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,25 @@
+package method;
+
+import java.util.Scanner;
+
+public class Even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int min,max;
+		Scanner sc = new Scanner(System.in); 
+		    System.out.println("enter no min vale");
+			min=sc.nextInt();
+			System.out.println("enter a max value");
+			max=sc.nextInt();
+		
+		while(min<=max) {
+			if(min%2==0)
+			{
+				System.out.println(min+"");
+			}
+			min++;
+		}
+	}
+
+}
